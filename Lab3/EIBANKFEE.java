8a2b3c0875f714014142c4e18b661bc4378a9707
Ten cua em <email@example.com>
2026-01-26 12:46:12 +0700

    EIBANKFEE

---
+++ Lab3/EIBANKFEE.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+ class EIBANKFEE {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double money = sc.nextDouble();
+        long checks = sc.nextLong();
+
+        double monthlyFee;
+        double checkFee;
+
+        if (money < 500) {
+            monthlyFee = 12.0;
+            checkFee = 0.20;
+        } else if (money < 2000) {
+            monthlyFee = 7.5;
+            checkFee = 0.20;
+        } else if (money < 5000) {
+            monthlyFee = 5.0;
+            checkFee = 0.10;
+        } else {
+            monthlyFee = 0.0;
+            checkFee = 0.0;
+        }
+
+        double total = monthlyFee + checks * checkFee;
+
+        System.out.print(Math.round((double)total*100)/100d);
+    }
+}
