fa8bb0e3c1564dbd8e2a01fe47367c76fbe9f982
Ten cua em <email@example.com>
2026-01-27 14:46:01 +0700

    EIVCHR3

---
+++ Lab5/EIVCHR3.java
@@ -0,0 +1,37 @@
+package Lab5;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class EIVCHR3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numOfItem = sc.nextInt();
+        int numOfVoucher = sc.nextInt();
+        
+        long[] prices = new long[numOfItem];
+        long totalMoney = 0;
+        
+        for (int i = 0; i < numOfItem; i++){
+            prices[i] = sc.nextLong();
+            totalMoney += prices[i];
+        }
+        
+        
+        Arrays.sort(prices);
+        
+        
+        long totalDiscount = 0;
+        for (int i = 0; i < numOfVoucher; i++) {
+            long itemPrice = prices[numOfItem - 1 - i]; 
+            long discount = (itemPrice * 3) / 10;  
+            if (discount > 50000) {
+                discount = 50000;
+            }
+            totalDiscount += discount;
+        }
+        
+        long amountToPay = totalMoney - totalDiscount;
+        System.out.println(amountToPay);
+    }
+}
