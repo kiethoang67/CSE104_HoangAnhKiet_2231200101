ea51f28ecb1297ffdc2e5a875ea130898b43b075
Ten cua em <email@example.com>
2026-01-27 14:20:57 +0700

    EIVCHR1

--- Lab5/EIVCHR1.java
+++ Lab5/EIVCHR1.java
@@ -6,11 +6,11 @@ public class EIVCHR1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         long money = sc.nextLong();
-        double moneyDiscount = money * 0.3;
-        if (moneyDiscount >= 50000) {
-            moneyDiscount =  50000;
+        long moneyDiscount = (money * 3) / 10;  
+        if (moneyDiscount > 50000) {
+            moneyDiscount = 50000;
         }
-        double actualMoney = money - moneyDiscount;
-        System.out.println(Math.round(actualMoney));
+        long actualMoney = money - moneyDiscount;
+        System.out.println(actualMoney);
     }
 }
---
+++ Lab5/EIVCHR2.java
@@ -0,0 +1,29 @@
+package Lab5;
+
+import java.util.Scanner;
+
+public class EIVCHR2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numOfItem = sc.nextInt();
+        long[] prices = new long[numOfItem];
+        long maxPrice = 0;
+        long totalMoney = 0;
+        
+        for (int i = 0; i < numOfItem; i++){
+            long money = sc.nextLong();
+            prices[i] = money;
+            totalMoney += money;
+            maxPrice = Math.max(maxPrice, money);
+        }
+        
+        // Apply 30% discount (max 50000) to the most expensive item
+        long discount = (maxPrice * 3) / 10;
+        if (discount > 50000) {
+            discount = 50000;
+        }
+        
+        long amountToPay = totalMoney - discount;
+        System.out.println(amountToPay);
+    }
+}
