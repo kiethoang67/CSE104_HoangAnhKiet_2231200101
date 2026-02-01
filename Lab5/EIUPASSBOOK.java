4ce35bf024d9a778bc1418f69c5aec3781d3e460
Ten cua em <email@example.com>
2026-02-01 22:06:28 +0700

    passbook

--- Lab5/EIUPASSBOOK.java
+++ Lab5/EIUPASSBOOK.java
@@ -1,25 +1,21 @@
-package Lab5;
-
 import java.util.Scanner;
 
 public class EIUPASSBOOK {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        long X = sc.nextLong(); // tien
+        int n = sc.nextInt(); // thang gui
+        int m = sc.nextInt(); // thang rut
+        double year = (n - m) / 12;
+        double month = (n - m) % 12;
+        double r1 = sc.nextDouble() / 100; // lai ngan hang
+        double r2 = sc.nextDouble() / 100; // lai rut
 
-        double X = sc.nextDouble(); 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        double r1 = sc.nextDouble();
-        double r2 = sc.nextDouble();
-
-        double option1 = X;
-        double S = X * Math.pow(1 + r1 / 100.0, n / 12.0);
-        double t = (n - m) / 12.0;
-        double option2 = S / (1 + r2 / 100.0 * t);
-
-        System.out.println(Math.round(Math.max(option1, option2)));
+        long option1 = X;
+        double tinh = X + (X * n * r1) / 12;
+        double option2 = tinh / (Math.pow(1 + r2, year) * (1 + (r2 * month) / 12));
+        double option = Math.max(option1, option2);
+        System.out.println(Math.round(option));
     }
 }
-
-
-
