d1e15c95da49fea580a43eba8bd474dd4215d5bd
Ten cua em <email@example.com>
2026-02-02 10:42:31 +0700

    saving2

---
+++ Lab5/EIAPP21222FQ2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class EIAPP21222FQ2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        double total = 0;
+        for (int i = m; i>0; i--){
+            double n = sc.nextDouble();
+            total+=cal(n, i);
+        }
+        System.out.println(Math.round(total));
+    }
+    public static double cal (double n, int m){
+        double[] r = { 0, 0.039, 0.0392, 0.0395, 0.0399, 0.0404, 0.0554, 0.0572, 0.0592, 0.0614, 0.0638, 0.0664, 0.0692 };
+        int year = m / 12;
+        int month = m % 12;
+        if (year > 0) {
+            n = n * Math.pow(1 + r[12] / 12 * 12, year);
+        }
+        if (month > 0) {
+            n = n * (1 + r[month] / 12 * month);
+        }
+        return n;
+    }
+}
