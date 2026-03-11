bcb7b8c4fb6f4ebc5bce56a7d6b0043a82a40bea
Ten cua em <email@example.com>
2026-03-11 21:53:23 +0700

    EIUFF2

---
+++ lab7/EIUFFF2.JAVA
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class EIUFF2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double r = (double)sc.nextDouble()/100;
+        double f = (double)sc.nextDouble()/100;
+        int C = sc.nextInt();
+
+        double tinh = C / (r - f);
+        System.out.println((int)Math.ceil(tinh));
+    }
+
+    
+}
